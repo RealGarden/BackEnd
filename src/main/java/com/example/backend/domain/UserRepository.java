75ceb2857125d08d869db43dccd44715670ba7ba
uzhjd <ujeong0528@gmail.com>
2021-09-09 18:13:31 +0900

    login

--- src/main/java/com/example/backend/domain/UserRepository.java
+++ src/main/java/com/example/backend/domain/UserRepository.java
@@ -1,4 +1,9 @@
-package domain;
+package com.example.backend.domain;
 
-public interface UserRepository {
+import com.example.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
